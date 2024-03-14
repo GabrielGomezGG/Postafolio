7aeccf02fda1cbc5e53c0ad71ec4d4c115acc9aa
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2024-03-14 15:11:29 -0300

    Actualizar diseño de página "Sobre mi" y eliminar componentes obsoletos

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button({ title, setPositionItem, index }) {
-    return (
-        <>
-            <button onClick={() => setPositionItem(index)}>{title}</button>
-        </>
-    )
-}
-
-export default Button
